799d2dd18c2add661830137dba62a04158e92b7c
kubackikacper <kacper1048@gmail.com>
2025-03-03 15:47:14 +0100

    working on validation

--- src/Components/CheckoutView.tsx
+++ src/Components/CheckoutView.tsx
@@ -16,14 +16,21 @@ interface IFormProps{
   email?:string,
   totalPrice?:number,
   orderDate?:Date,
-  status?: string,
-  paymentId?:number
+  address?: string,
+  paymentMethod?:string,
+  status?:string,
+  cardName?:string,
+  cardNumber?:string,
+  cardCVV?:string,
+  cardExpirationDate?:Date
 }
 
 const CheckoutView = () => {
   const {cartItems,products} = useShoppingCart()
-  const [formStateValues, setFormStateValues] = useState<IFormProps | undefined>(undefined);
+  const [formStateValues, setFormStateValues] = useState<IFormProps | null>(null);
   const [checkoutClicked, setCheckoutClicked] = useState<boolean>(false)
+  const [validated,setValidated] = useState<boolean>(false);
+
   const notify=()=>{
     toast.info('Order placed successfully! Thank you!', {
     position: "top-left",
@@ -55,14 +62,27 @@ const CheckoutView = () => {
 
   
   const handleSubmit =(e:React.FormEvent<HTMLFormElement>)=>{
-    setCheckoutClicked(true)
+    const form = e.currentTarget;
+    if (form.checkValidity()===false)
+      {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+    else
+    {
+      
     e.preventDefault();
+    setCheckoutClicked(true)
+    console.log(formStateValues)
     
+    setValidated(true)
+
     if(formStateValues)
     {
       formStateValues.totalPrice = Math.round(checkoutTotal*100)/100;
       formStateValues.orderDate = new Date(Date.now());
-      
+      formStateValues.paymentMethod = "Card";
+      formStateValues.status = "Pending";
     }
     fetch(apiUrls.placeOrderUrl.urlLink,{
       method:'POST',
@@ -78,12 +98,11 @@ const CheckoutView = () => {
       
     })
 
-  }
+  }}
   
   const handleChange =(e:React.ChangeEvent<HTMLInputElement >)=>{
     setFormStateValues({...formStateValues,[e.target.name]:e.target.value})
     
-    console.log(formStateValues)
   }
  
   return (
@@ -91,45 +110,49 @@ const CheckoutView = () => {
       {cartItems.length===0 ? <div className="alert alert-danger">NO ITEMS IN CART, CANNOT PROCEED TO CHECKOUT</div> :
         <Container>
           <p style={{fontSize:"20px"}}>Please provide informations below</p>
-          <Form onSubmit={handleSubmit}>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Row>
                 <Col>
                 <Row className="justify-content-md-center mb-3">
                   <Col>
+                  <Form.Group controlId='email'>
                     <Form.Label>E-Mail</Form.Label>
-                    <Form.Control required type="text" placeholder="Enter Your e-mail" name={"email"} onChange={handleChange}/>
+                    <Form.Control required type="text" placeholder="Enter Your e-mail" name={"email"} 
+                    onChange={handleChange} value={formStateValues?.email} isInvalid={validated && !/^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$/.test(formStateValues?.email || '')}/>
+                    <Form.Control.Feedback type="invalid">Please enter a valid username (alphanumeric characters only).</Form.Control.Feedback>
+                    </Form.Group>
                   </Col>
                 </Row>
                 <Row className="justify-content-md-center mb-3">
                   <Col>
                     <Form.Label>Address</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Your address" required name={"status"} onChange={handleChange}/>
+                    <Form.Control type="text" placeholder="Enter Your address" required name={"address"} onChange={handleChange}/>
                   </Col>
                 </Row>
                 <Row className="justify-content-md-center mb-3">
                   <Col>
                     <Form.Label>Card Information</Form.Label>
-                    <Form.Control type="text" placeholder="Enter name on card" />
+                    <Form.Control type="text" placeholder="Enter name on card" name={"cardName"} onChange={handleChange}/>
                   </Col>
                   <Col>
                     <Form.Label>Credit card number</Form.Label>
-                    <Form.Control type="text" placeholder="Enter credit card number" />
+                    <Form.Control type="text" placeholder="Enter credit card number" name={"cardNumber"} onChange={handleChange}/>
                   </Col>
                 </Row>
                 <Row className="justify-content-md-center mb-3">
                   <Col>
                     <Form.Label>Expiration Date</Form.Label>
-                    <Form.Control type="date" />
+                    <Form.Control type="date" name={"cardExpirationDate"} onChange={handleChange}/>
                   </Col>
                   <Col>
                     <Form.Label>CVV</Form.Label>
-                    <Form.Control type="text" placeholder="Enter CVV number" />
+                    <Form.Control type="text" placeholder="Enter CVV number" name={"cardCVV"} onChange={handleChange}/>
                   </Col>
                 </Row>
                 <Row className="">
                   <Col className='mr-5'>
                     <Button type='submit' disabled={checkoutClicked} style={{ background: "violet", border: "none", marginRight:"3px" }}>Proceed</Button>
-                    <Button style={{ background: "gray", border: "none" }} onClick={()=>GoBackButton()}>Go Back</Button>
+                    <Button disabled={checkoutClicked} style={{ background: "gray", border: "none" }} onClick={()=>GoBackButton()}>Go Back</Button>
                   </Col>
                 </Row>
               </Col>
